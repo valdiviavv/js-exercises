0d7f2571815546ce685ef9ee08d4890bf23b7490
veronica <valdiviavv@hotmail.com>
2022-02-24 09:58:37 -0500

    initial commit

---
+++ variables.js
@@ -0,0 +1,17 @@
+
+let color ='black';
+
+console.log('My favorite color is ' + color + '!');
+
+console.log('---------------------');
+
+
+
+let number = 42;
+
+//number = number + 1;
+number += 1;
+
+console.log(number);
+
+
